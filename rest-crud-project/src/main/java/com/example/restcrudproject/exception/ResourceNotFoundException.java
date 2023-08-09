96feaa7ef8c4a8d76ef93f087dab5a18ed0404dc
dannochan <62658862+dannochan@users.noreply.github.com>
2023-08-09 16:36:27 +0200

    Update ResourceNotFoundException.java

--- rest-crud-project/src/main/java/com/example/restcrudproject/exception/ResourceNotFoundException.java
+++ rest-crud-project/src/main/java/com/example/restcrudproject/exception/ResourceNotFoundException.java
@@ -1,6 +1,9 @@
 package com.example.restcrudproject.exception;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     private static final long SerialVersionUID = 1L;
