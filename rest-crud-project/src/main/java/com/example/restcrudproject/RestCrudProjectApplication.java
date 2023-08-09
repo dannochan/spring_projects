1b01749cca753b3eec130636c059fda83ef4ef32
dannochan <62658862+dannochan@users.noreply.github.com>
2023-08-09 15:55:17 +0200

    Initial commit

---
+++ rest-crud-project/src/main/java/com/example/restcrudproject/RestCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.restcrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestCrudProjectApplication.class, args);
+    }
+
+}
