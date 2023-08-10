8da5604b6a9cf78ec0b89689c048b8965a37272f
dannochan <62658862+dannochan@users.noreply.github.com>
2023-08-10 10:36:56 +0200

    student entity created

---
+++ student-mgt-sys/src/main/java/com/example/studentmgtsys/entity/Student.java
@@ -0,0 +1,22 @@
+package com.example.studentmgtsys.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="first_name", nullable = false)
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+}
