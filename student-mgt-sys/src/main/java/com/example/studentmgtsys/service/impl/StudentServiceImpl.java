528ffd6de89b8869576975d4ce56cd41538ee775
dannochan <62658862+dannochan@users.noreply.github.com>
2023-08-10 10:49:23 +0200

    service implementation added

---
+++ student-mgt-sys/src/main/java/com/example/studentmgtsys/service/impl/StudentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.studentmgtsys.service.impl;
+
+import com.example.studentmgtsys.entity.Student;
+import com.example.studentmgtsys.service.StudentService;
+
+import java.util.List;
+
+public class StudentServiceImpl implements StudentService {
+    @Override
+    public List<Student> getAllStudent() {
+        return null;
+    }
+
+    @Override
+    public void addStudent(Student student) {
+
+    }
+
+    @Override
+    public Student updateStudent(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteStudent(long id) {
+
+    }
+}
