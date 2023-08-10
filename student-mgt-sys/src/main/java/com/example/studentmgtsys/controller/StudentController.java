03e291bf7a3813efa9f29236686424b47b64bc7b
dannochan <62658862+dannochan@users.noreply.github.com>
2023-08-10 11:32:08 +0200

    get student list method in controller

--- student-mgt-sys/src/main/java/com/example/studentmgtsys/controller/StudentController.java
+++ student-mgt-sys/src/main/java/com/example/studentmgtsys/controller/StudentController.java
@@ -1,7 +1,26 @@
 package com.example.studentmgtsys.controller;
 
+import com.example.studentmgtsys.service.impl.StudentServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
+    private StudentServiceImpl studentService;
+
+    public StudentController( StudentServiceImpl studentService){
+        super();
+        this.studentService=studentService;
+    }
+
+    // a method receive a student list and display
+    @GetMapping("/")
+    public String studentList(
+            Model page
+    ){
+    page.addAttribute("students", studentService.getAllStudents());
+
+        return "students";
+    }
 }
