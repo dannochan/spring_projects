6d61e45a306d142acfeef852b667acdc1ffe62a7
dannochan <62658862+dannochan@users.noreply.github.com>
2023-08-13 13:53:45 +0200

    rest data

---
+++ mgt-system-login/src/main/java/com/example/mgtsystemlogin/MgtSystemLoginApplication.java
@@ -0,0 +1,14 @@
+package com.example.mgtsystemlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class MgtSystemLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgtSystemLoginApplication.class, args);
+    }
+
+}
---
+++ mgt-system-login/src/test/java/com/example/mgtsystemlogin/MgtSystemLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mgtsystemlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgtSystemLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
